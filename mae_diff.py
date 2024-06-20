528d1714047e3cd87e7e98b1bef3c96e5cb30a5a
O H <ohoidn@slac.stanford.edu>
2024-06-20 18:40:27 -0400

    save pre-forward map model layer

--- mae_diff.py
+++ mae_diff.py
@@ -32,14 +32,11 @@ class FlatDirectoryDataset(Dataset):
         return image
 
 def load_datasets_and_dataloaders(train_dir, test_dir, batch_size=128, num_workers=4):
-    # Define the transform without normalization
     transform = Compose([ToTensor()])
 
-    # Create the custom datasets
     train_dataset = FlatDirectoryDataset(root_dir=train_dir, transform=transform)
     test_dataset = FlatDirectoryDataset(root_dir=test_dir, transform=transform)
 
-    # DataLoaders
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
     
@@ -70,24 +67,18 @@ if __name__ == '__main__':
     assert batch_size % load_batch_size == 0
     steps_per_update = batch_size // load_batch_size
 
-#    train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
-#    val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor(), Normalize(0.5, 0.5)]))
     train_dataset, val_dataset, dataloader, test_dataloader = load_datasets_and_dataloaders(
         train_dir='diffracted_images/train',
         test_dir='diffracted_images/test',
-        batch_size = batch_size
+        batch_size=batch_size
     )
-#    train_dataset = torchvision.datasets.CIFAR10('data', train=True, download=True, transform=Compose([ToTensor()]))
-#    val_dataset = torchvision.datasets.CIFAR10('data', train=False, download=True, transform=Compose([ToTensor()]))
-#    dataloader = torch.utils.data.DataLoader(train_dataset, load_batch_size, shuffle=True, num_workers=4)
-
 
     writer = SummaryWriter(os.path.join('logs', 'cifar10', 'mae-pretrain'))
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     probe = (probe).to(device)
 
-    model = MAE_ViT(mask_ratio=args.mask_ratio, intensity_scale = intensity_scale,
-                    probe = probe).to(device)
+    model = MAE_ViT(mask_ratio=args.mask_ratio, intensity_scale=intensity_scale,
+                    probe=probe).to(device)
     optim = torch.optim.AdamW(model.parameters(), lr=args.base_learning_rate * args.batch_size / 256, betas=(0.9, 0.95), weight_decay=args.weight_decay)
     lr_func = lambda epoch: min((epoch + 1) / (args.warmup_epoch + 1e-8), 0.5 * (math.cos(epoch / args.total_epoch * math.pi) + 1))
     lr_scheduler = torch.optim.lr_scheduler.LambdaLR(optim, lr_lambda=lr_func, verbose=True)
@@ -108,30 +99,22 @@ if __name__ == '__main__':
                 optim.step()
                 optim.zero_grad()
             losses.append(loss.item())
-            
-            # Calculate running average loss
-#            running_loss += loss.item()
-#            if i % 10 == 0 or i < 5:  # Print every 10 batches
-#                avg_running_loss = running_loss / 10
-#                print(f"Epoch [{e+1}/{args.total_epoch}], Batch [{i}/{len(dataloader)}], Running Average Loss: {avg_running_loss:.4f}")
-#                running_loss = 0.0
-        
+
         lr_scheduler.step()
         avg_loss = sum(losses) / len(losses)
         writer.add_scalar('mae_loss', avg_loss, global_step=e)
         print(f'In epoch {e}, average training loss is {avg_loss}.')
 
-        ''' visualize the first 16 predicted images on val dataset'''
+        ''' visualize the first 16 predicted images on val dataset '''
         model.eval()
         with torch.no_grad():
             val_img = torch.stack([val_dataset[i] for i in range(16)])
             val_img = val_img.to(device)
-            predicted_val_img, mask = model(val_img)
+            predicted_val_img, mask, intermediate_img = model.forward_with_intermediate(val_img)
             predicted_val_img = predicted_val_img * mask + val_img * (1 - mask)
             img = torch.cat([val_img * (1 - mask), predicted_val_img, val_img], dim=0)
             img = rearrange(img, '(v h1 w1) c h w -> c (h1 h) (w1 v w)', w1=2, v=3)
             writer.add_image('mae_image', (img + 1) / 2, global_step=e)
-        
+
         ''' save model '''
         torch.save(model, args.model_path)
-
